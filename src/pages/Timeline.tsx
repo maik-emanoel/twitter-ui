3e4fe9fb84518111da81e52a66f62ae2b847b407
Maik Emanoel <maikemanoel11@gmail.com>
2023-07-14 13:32:07 -0300

    feat: add function to create newTweet

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,30 +1,51 @@
-import { Header } from "../components/Header"
-import { Separator } from "../components/Separator"
-import { Tweet } from "../components/Tweet"
-
-const tweets = ["Meu primeiro tweet", "Teste", "Testandoooooooooo"];
+import { FormEvent, useState } from "react";
+import { Header } from "../components/Header";
+import { Separator } from "../components/Separator";
+import { Tweet } from "../components/Tweet";
 
 export function Timeline() {
-    return (
-        <main>
-          <Header title="Home" />
+  const [newTweet, setNewTweet] = useState('')
+  const [tweets, setTweets] = useState(["Meu primeiro tweet", "Teste", "Testandoooooooooo"])
 
-          <form className='py-6 px-5 flex flex-col gap-2'>
-            <label htmlFor="tweet" className='flex gap-3'>
-              <img src="https://github.com/maik-emanoel.png" alt="Maik Emanoel" className='w-12 h-12 rounded-full'/>
-              <textarea id='tweet' placeholder="What's happening?" className='flex-1 text-xl font-medium mt-3 resize-none focus:outline-none placeholder:text-[#5b7073]'/>
-            </label>
+  function createNewTweet(e: FormEvent) {
+    e.preventDefault();
+    setTweets([newTweet, ...tweets])
+    setNewTweet('')
+  }
 
-            <button type='submit' className='ml-auto bg-twitterBlue rounded-full py-3 px-6 text-white font-black hover:brightness-90'>Tweet</button>
-          </form>
+  return (
+    <main>
+      <Header title="Home" />
 
-          <Separator />
+      <form onSubmit={createNewTweet} className="py-6 px-5 flex flex-col gap-2">
+        <label htmlFor="tweet" className="flex gap-3">
+          <img
+            src="https://github.com/maik-emanoel.png"
+            alt="Maik Emanoel"
+            className="w-12 h-12 rounded-full"
+          />
+          <textarea
+            id="tweet"
+            placeholder="What's happening?"
+            className="flex-1 text-xl font-medium mt-3 resize-none focus:outline-none placeholder:text-[#5b7073]"
+            value={newTweet}
+            onChange={(e) => setNewTweet(e.target.value)}
+          />
+        </label>
 
-          {
-            tweets.map(tweet => {
-              return <Tweet key={tweet} content={tweet} />
-            })
-          }
-        </main>
-    )
+        <button
+          type="submit"
+          className="ml-auto bg-twitterBlue rounded-full py-3 px-6 text-white font-black hover:brightness-90"
+        >
+          Tweet
+        </button>
+      </form>
+
+      <Separator />
+
+      {tweets.map((tweet) => {
+        return <Tweet key={tweet} content={tweet} />;
+      })}
+    </main>
+  );
 }
