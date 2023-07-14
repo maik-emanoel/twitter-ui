01b505625beafa23117265e25eb2903810150c97
Maik Emanoel <maikemanoel11@gmail.com>
2023-07-14 10:31:48 -0300

    move on some codes into Default component

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import { Sidebar } from "./components/Sidebar";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className="mx-auto max-w-[1000px] grid grid-cols-[300px_1fr]">
-      <Sidebar />
-
-      <div className="border-l-[1px] border-r-[1px] border-grayBorder">
-        <RouterProvider router={router} />
-      </div>
-    </div>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
