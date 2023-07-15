06a7d107df8771a76cb5b2c9dfc7801ff80aa534
Maik Emanoel <maikemanoel11@gmail.com>
2023-07-15 15:52:44 -0300

    set class dark & add some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,16 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         twitterBlue: '#1da1f2',
         grayBorder: '#ebeef0',
-        separatorColor: '#f7f9fa'
+        grayBorderDark: '#2F2F2F',
+        separatorColor: '#f7f9fa',
+        bodyDark: '#121214',
+        tweetColor: '#E1E1E6'
       }
     },
     screens: {
