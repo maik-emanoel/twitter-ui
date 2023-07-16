841332cf49d9689cd8d5330643adb0d1629ba1b9
Maik Emanoel <maikemanoel11@gmail.com>
2023-07-16 10:25:36 -0300

    feat: create function to toggle theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,37 @@
-import { Sparkle } from '@phosphor-icons/react'
+import { Moon, Sparkle, SunDim } from "@phosphor-icons/react";
+import { useState } from "react";
 
 interface HeaderProps {
-    title: string
+  title: string;
 }
 
-export function Header( {title} :HeaderProps) {
-    return (
-        <div className='py-6 px-5 flex items-center justify-between text-xl font-bold border-b-[1px] border-b-grayBorder dark:border-b-grayBorderDark dark:text-tweetColor'>
-            {title}
-            <Sparkle className='w-6 h-6 text-twitterBlue' />
-        </div>
-    )
+export function Header({ title }: HeaderProps) {
+  const [isDark, setIsDark] = useState(false);
+
+  function handleToggleTheme() {
+    const html = document.querySelector("html");
+    setIsDark((prevState) => !prevState);
+
+    if (isDark) {
+      html?.classList.remove("dark");
+    } else {
+      html?.classList.add("dark");
+    }
+  }
+
+  return (
+    <div className="py-6 px-5 flex items-center justify-between text-xl font-bold border-b-[1px] border-b-grayBorder dark:border-b-grayBorderDark dark:text-tweetColor">
+      {title}
+      <div className="flex items-center gap-2">
+        <button onClick={handleToggleTheme}>
+          {isDark ? (
+            <SunDim size={24} className="text-twitterBlue" />
+          ) : (
+            <Moon size={24} className="text-twitterBlue" />
+          )}
+        </button>
+        <Sparkle className="w-6 h-6 text-twitterBlue" />
+      </div>
+    </div>
+  );
 }
