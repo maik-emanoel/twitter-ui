c8e017814f7e13909743ce79b3ed815f9a999007
Maik Emanoel <maikemanoel11@gmail.com>
2023-07-19 09:38:33 -0300

    set one column to the display grid

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components/Sidebar";
+import { Bottombar } from "../components/Bottombar";
 
 export function Default() {
     return (
-        <div className="mx-auto max-w-[1000px] grid grid-cols-[300px_1fr] md:grid-cols-[80px_1fr] min-h-screen">
+        <div className="mx-auto max-w-[1000px] grid grid-cols-[300px_1fr] md:grid-cols-[80px_1fr] min-h-screen sm:grid-cols-1">
         <Sidebar />
+        <Bottombar />
   
         <div className="border-l-[1px] border-r-[1px] border-grayBorder dark:border-grayBorderDark">
             <Outlet />
