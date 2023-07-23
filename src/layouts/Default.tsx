1f95e834ebe02184897fb5a4c488825502df4473
Maik Emanoel <maikemanoel11@gmail.com>
2023-07-23 13:30:25 -0300

    add SidebarProvider

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -2,9 +2,11 @@ import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components/Sidebar";
 import { Bottombar } from "../components/Bottombar";
 import { Pencil } from "@phosphor-icons/react";
+import { SidebarProvider } from "../context/SidebarContext";
 
 export function Default() {
   return (
+    <SidebarProvider>
       <div className="mx-auto max-w-[1000px] grid grid-cols-[300px_1fr] md:grid-cols-[80px_1fr] min-h-screen sm:grid-cols-1">
         <Sidebar />
 
@@ -17,5 +19,6 @@ export function Default() {
 
         <Bottombar />
       </div>
+    </SidebarProvider>
   );
 }
