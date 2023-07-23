6f8785f80b94096445cf1f41b39547b27d513abd
Maik Emanoel <maikemanoel11@gmail.com>
2023-07-23 13:23:40 -0300

    refactor

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -4,17 +4,18 @@ import { Bottombar } from "../components/Bottombar";
 import { Pencil } from "@phosphor-icons/react";
 
 export function Default() {
-    return (
-        <div className="mx-auto max-w-[1000px] grid grid-cols-[300px_1fr] md:grid-cols-[80px_1fr] min-h-screen sm:grid-cols-1">
+  return (
+      <div className="mx-auto max-w-[1000px] grid grid-cols-[300px_1fr] md:grid-cols-[80px_1fr] min-h-screen sm:grid-cols-1">
         <Sidebar />
-        <Bottombar />
-  
+
         <div className="border-l-[1px] border-r-[1px] border-grayBorder dark:border-grayBorderDark">
-            <Outlet />
-            <button className="fixed right-4 bottom-20 bg-twitterBlue w-14 h-14 rounded-full shadow-floatButton hidden sm:block">
-                <Pencil size={24} color="white" className="mx-auto" />
-            </button>
+          <Outlet />
+          <button className="fixed right-4 bottom-20 bg-twitterBlue w-14 h-14 rounded-full shadow-floatButton hidden sm:block">
+            <Pencil size={24} color="white" className="mx-auto" />
+          </button>
         </div>
+
+        <Bottombar />
       </div>
-    )
+  );
 }
