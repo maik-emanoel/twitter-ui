0e78d66b8c7670e8e5852aaf7776b9d292e8a082
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-01 11:34:45 -0300

    refactor

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,58 +1,27 @@
-import { FormEvent, KeyboardEvent, useState } from "react";
 import { Header } from "../components/Header";
 import { Separator } from "../components/Separator";
 import { Tweet } from "../components/Tweet";
+import { CreateTweetForm } from "../components/CreateTweetForm";
+import { useState } from "react";
+import { Pencil } from "@phosphor-icons/react";
 
 export function Timeline() {
-  const [newTweet, setNewTweet] = useState('')
-  const [tweets, setTweets] = useState(["Meu primeiro tweet", "Teste", "Testandoooooooooo"])
+  const [tweets, setTweets] = useState([
+    "Meu primeiro tweet",
+    "Teste",
+    "Testandoooooooooo",
+  ]);
+  const [isTweetFormVisible, setIsTweetFormVisible] = useState(false)
 
-  function createNewTweet(e: FormEvent) {
-    e.preventDefault();
-    if(newTweet === '') return
-
-    setTweets([newTweet, ...tweets])
-    setNewTweet('')
-  }
-
-  function handleHotKeySubmit(e: KeyboardEvent) {
-    if(newTweet === '') return
-
-    if(e.key === "Enter" && (e.ctrlKey || e.metaKey)) {
-      setTweets([newTweet, ...tweets])
-      setNewTweet('')
-    }
+  function handleShowTweetForm() {
+    setIsTweetFormVisible(true)
   }
 
   return (
     <main>
       <Header title="Home" />
 
-      <form onSubmit={createNewTweet} className="py-6 px-5 flex flex-col gap-2">
-        <label htmlFor="tweet" className="flex gap-3">
-          <img
-            src="https://github.com/maik-emanoel.png"
-            alt="Maik Emanoel"
-            className="w-12 h-12 rounded-full"
-          />
-          <textarea
-            id="tweet"
-            placeholder="What's happening?"
-            className="flex-1 text-xl font-medium mt-3 resize-none focus:outline-none placeholder:text-[#5b7073] placeholder:dark:text-[#828282]"
-            value={newTweet}
-            onKeyDown={handleHotKeySubmit}
-            onChange={(e) => setNewTweet(e.target.value)}
-          />
-        </label>
-
-        <button
-          type="submit"
-          className="ml-auto bg-twitterBlue rounded-full py-3 px-6 text-white font-black transition-all duration-300 ease-in-out hover:brightness-90 disabled:brightness-75"
-          disabled={newTweet === '' ? true : false}
-        >
-          Tweet
-        </button>
-      </form>
+      <CreateTweetForm tweets={tweets} setTweets={setTweets} isTweetFormVisible={isTweetFormVisible} setIsTweetFormVisible={setIsTweetFormVisible}/>
 
       <Separator />
 
@@ -61,6 +30,10 @@ export function Timeline() {
           return <Tweet key={tweet} content={tweet} />;
         })}
       </div>
+
+      <button className="fixed right-4 bottom-20 bg-twitterBlue w-14 h-14 rounded-full shadow-floatButton hidden sm:block" onClick={handleShowTweetForm}>
+        <Pencil size={24} color="white" className="mx-auto" />
+      </button>
     </main>
   );
 }
