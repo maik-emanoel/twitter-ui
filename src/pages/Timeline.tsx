fcd3b134a08cc2b14f1c72e2495b8b462d9d9f16
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-03 11:13:57 -0300

    change tweets variable & refactor

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -5,33 +5,68 @@ import { CreateTweetForm } from "../components/CreateTweetForm";
 import { useState } from "react";
 import { Pencil } from "@phosphor-icons/react";
 
+export interface TweetProps {
+  userAvatar: string;
+  userName: string;
+  userLogin: string;
+  content: string;
+  hasImage?: boolean;
+}
+
 export function Timeline() {
-  const [tweets, setTweets] = useState([
-    "Meu primeiro tweet",
-    "Teste",
-    "Testandoooooooooo",
+  const [isTweetFormVisible, setIsTweetFormVisible] = useState(false);
+  const [tweets, setTweets] = useState<TweetProps[]>([
+    {
+      userAvatar: "https://github.com/maik-emanoel.png",
+      userName: "Maik Emanoel",
+      userLogin: "maik_emanoel",
+      content: "Meu primeiro tweet",
+      hasImage: false,
+    },
+    {
+      userAvatar: "https://github.com/diego3g.png",
+      userName: "Diego Fernandes",
+      userLogin: "diego_3g",
+      content: "Teste teste",
+      hasImage: false,
+    },
   ]);
-  const [isTweetFormVisible, setIsTweetFormVisible] = useState(false)
 
   function handleShowTweetForm() {
-    setIsTweetFormVisible(true)
+    setIsTweetFormVisible(true);
   }
 
   return (
     <main>
       <Header title="Home" />
 
-      <CreateTweetForm tweets={tweets} setTweets={setTweets} isTweetFormVisible={isTweetFormVisible} setIsTweetFormVisible={setIsTweetFormVisible}/>
+      <CreateTweetForm
+        tweets={tweets}
+        setTweets={setTweets}
+        isTweetFormVisible={isTweetFormVisible}
+        setIsTweetFormVisible={setIsTweetFormVisible}
+      />
 
       <Separator />
 
       <div className="sm:mb-12">
-        {tweets.map((tweet) => {
-          return <Tweet key={tweet} content={tweet} />;
+        {tweets.map((tweet, index) => {
+          return (
+            <Tweet
+              key={index}
+              userAvatar={tweet.userAvatar}
+              userName={tweet.userName}
+              userLogin={tweet.userLogin}
+              content={tweet.content}
+              hasImage={tweet.hasImage} />
+          );
         })}
       </div>
 
-      <button className="fixed right-4 bottom-20 bg-twitterBlue w-14 h-14 rounded-full shadow-floatButton hidden sm:block" onClick={handleShowTweetForm}>
+      <button
+        className="fixed right-4 bottom-20 bg-twitterBlue w-14 h-14 rounded-full shadow-floatButton hidden sm:block"
+        onClick={handleShowTweetForm}
+      >
         <Pencil size={24} color="white" className="mx-auto" />
       </button>
     </main>
