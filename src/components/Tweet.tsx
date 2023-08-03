425fe135393446d5be44400f4e97fba2dc617b6f
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-03 11:12:25 -0300

    pass new props to tweet

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,29 +1,31 @@
 import { ArrowsClockwise, ChatCircle, Heart } from "@phosphor-icons/react";
 import { Link } from "react-router-dom";
+import { TweetProps } from "../pages/Timeline";
 
-interface TweetProps {
-  content: string;
-}
-
-export function Tweet({ content }: TweetProps) {
+export function Tweet({ userAvatar, userName, userLogin, content, hasImage }: TweetProps) {
   return (
     <Link
       to="/status"
       className="w-full py-6 px-5 grid grid-cols-[3rem_1fr] gap-3 border-b-[1px] border-grayBorder dark:border-grayBorderDark"
     >
       <img
-        src="https://github.com/maik-emanoel.png"
-        alt="Maik Emanoel"
+        src={userAvatar}
+        alt={userName}
         className="w-12 h-12 rounded-full"
       />
 
       <div className="flex flex-col gap-2">
         <div className="flex items-center gap-1">
-          <strong>Maik Emanoel</strong>
-          <span className="text-sm text-[#89a2b8] dark:text-[#828282]">@maik_emanoel</span>
+          <strong>{userName}</strong>
+          <span className="text-sm text-[#89a2b8] dark:text-[#828282]">@{userLogin}</span>
         </div>
 
-        <p className="leading-5 dark:text-tweetColor max-w-[600px] break-words">{content}</p>
+        <div>
+         <p className="leading-5 dark:text-tweetColor max-w-[600px] break-words">{content}</p>
+         {hasImage && (
+          <div>OI</div>
+         )}
+        </div>
 
         <div className="flex items-center gap-12 mt-3">
           <button className="flex items-center gap-2 text-sm text-[#89a2b8] hover:text-twitterBlue">
