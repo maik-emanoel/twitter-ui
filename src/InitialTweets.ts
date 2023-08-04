063407b5027644ce49431a1cc62f363da6c32c76
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-04 14:01:48 -0300

    create initial tweets

---
+++ src/InitialTweets.ts
@@ -0,0 +1,23 @@
+export const initialTweets = [
+  {
+    userAvatar: "https://github.com/maik-emanoel.png",
+    userName: "Maik Emanoel",
+    userLogin: "maik_emanoel",
+    content: "Meu primeiro tweet",
+    imageUrl: "https://github.com/maik-emanoel.png",
+  },
+  {
+    userAvatar: "https://github.com/diego3g.png",
+    userName: "Diego Fernandes",
+    userLogin: "diego_3g",
+    content: "Teste teste",
+    imageUrl: "https://source.unsplash.com/random",
+  },
+  {
+    userAvatar: "https://github.com/maykbrito.png",
+    userName: "Mayk Brito",
+    userLogin: "maykbrito",
+    content: "Falaaaaaa, dev!",
+    imageUrl: undefined,
+  },
+];
