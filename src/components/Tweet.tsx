58a1359903d73052b9aee473e2a1fe86ed615f65
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-05 16:42:11 -0300

    fix problem with responsivity

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -18,9 +18,9 @@ export function Tweet({
   return (
     <Link
       to="/status"
-      className="w-full py-6 px-5 grid grid-cols-[3rem_1fr] gap-3 border-b-[1px] border-grayBorder dark:border-grayBorderDark"
+      className="w-full py-6 px-5 grid grid-cols-[max-content_1fr] gap-3 border-b-[1px] border-grayBorder dark:border-grayBorderDark"
     >
-      <img src={userAvatar} alt={userName} className="w-12 h-12 rounded-full" />
+      <img src={userAvatar} alt={userName} className="w-10 h-10 rounded-full" />
 
       <div className="flex flex-col gap-2 max-w-[500px]">
         <div className="flex items-center gap-1">
@@ -46,7 +46,7 @@ export function Tweet({
         </div>
 
         <div
-          className="flex items-center gap-12 mt-3 justify-between h-5"
+          className="flex items-center mt-3 justify-between h-5"
           onClick={(e) => e.preventDefault()}
         >
           <button className="flex items-center gap-2 text-sm text-[#89a2b8] hover:text-twitterBlue group">
@@ -70,14 +70,14 @@ export function Tweet({
             <span className="pl-1">20</span>
           </button>
 
-          <button className="flex items-center gap-2 text-sm text-[#89a2b8] hover:text-twitterBlue group">
+          <button className="flex items-center gap-2 text-sm text-[#89a2b8] hover:text-twitterBlue group sm:hidden">
             <div className="w-[34.75px] h-[34.75px] grid place-items-center rounded-full -m-2 transition-colors duration-200 group-hover:bg-twitterBlue/10">
               <ChartLine size={18.75} />
             </div>
             <span className="pl-1">999</span>
           </button>
 
-          <button className="flex items-center gap-2 text-sm text-[#89a2b8] hover:text-twitterBlue group">
+          <button className="flex items-center gap-2 text-sm text-[#89a2b8] hover:text-twitterBlue group sm:hidden">
             <div className="w-[34.75px] h-[34.75px] grid place-items-center rounded-full -m-2 transition-colors duration-200 group-hover:bg-twitterBlue/10">
               <Export size={18.75} />
             </div>
