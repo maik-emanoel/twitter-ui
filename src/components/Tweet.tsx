c3105cc0b0ee673ef03b7194efb110e26e2cf427
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-07 10:11:45 -0300

    pass new props & refactor some code

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -14,6 +14,9 @@ export function Tweet({
   userLogin,
   content,
   imageUrl,
+  comments,
+  retweets,
+  likes,
 }: TweetProps) {
   return (
     <Link
@@ -22,7 +25,7 @@ export function Tweet({
     >
       <img src={userAvatar} alt={userName} className="w-10 h-10 rounded-full" />
 
-      <div className="flex flex-col gap-2 max-w-[500px]">
+      <div className="flex flex-col gap-[2px] max-w-[500px]">
         <div className="flex items-center gap-1">
           <strong>{userName}</strong>
           <span className="text-sm text-[#89a2b8] dark:text-[#828282]">
@@ -31,11 +34,14 @@ export function Tweet({
         </div>
 
         <div>
-          <p className="leading-5 dark:text-tweetColor max-w-[600px] break-words">
+          <p
+            className="leading-5 dark:text-tweetColor w-full whitespace-pre-line"
+            style={{ overflowWrap: "anywhere" }}
+          >
             {content}
           </p>
           {imageUrl && (
-            <div className="max-w-[500px] w-full mt-2">
+            <div className="max-w-[500px] w-full mt-2 min-w-0">
               <img
                 src={imageUrl}
                 alt="Imagem aleatória"
@@ -53,28 +59,30 @@ export function Tweet({
             <div className="w-[34.75px] h-[34.75px] grid place-items-center rounded-full -m-2 transition-colors duration-200 group-hover:bg-twitterBlue/10">
               <ChatCircle size={18.75} />
             </div>
-            <span className="pl-1">20</span>
+            <span className="pl-1">{comments ? comments : "01"}</span>
           </button>
 
           <button className="flex items-center gap-2 text-sm text-[#89a2b8] hover:text-retweetGreen group">
             <div className="w-[34.75px] h-[34.75px] grid place-items-center rounded-full -m-2 transition-colors duration-200 group-hover:bg-retweetGreen/10">
               <ArrowsClockwise size={18.75} />
             </div>
-            <span className="pl-1">20</span>
+            <span className="pl-1">
+              {retweets ? String(retweets).padStart(2, "0") : "09"}
+            </span>
           </button>
 
           <button className="flex items-center gap-2 text-sm text-[#89a2b8] hover:text-likePink group">
             <div className="w-[34.75px] h-[34.75px] grid place-items-center rounded-full -m-2 transition-colors duration-200 group-hover:bg-likePink/10">
               <Heart size={18.75} />
             </div>
-            <span className="pl-1">20</span>
+            <span className="pl-1">{likes ? likes : 2004}</span>
           </button>
 
           <button className="flex items-center gap-2 text-sm text-[#89a2b8] hover:text-twitterBlue group sm:hidden">
             <div className="w-[34.75px] h-[34.75px] grid place-items-center rounded-full -m-2 transition-colors duration-200 group-hover:bg-twitterBlue/10">
               <ChartLine size={18.75} />
             </div>
-            <span className="pl-1">999</span>
+            <span className="pl-1">1.2k</span>
           </button>
 
           <button className="flex items-center gap-2 text-sm text-[#89a2b8] hover:text-twitterBlue group sm:hidden">
