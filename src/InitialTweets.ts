c66c25422ad45c6fb6cae06cc69e47412e9a9c91
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-07 10:12:17 -0300

    add news tweets

--- src/InitialTweets.ts
+++ src/InitialTweets.ts
@@ -4,20 +4,67 @@ export const initialTweets = [
     userName: "Maik Emanoel",
     userLogin: "maik_emanoel",
     content: "Meu primeiro tweet",
-    imageUrl: "https://github.com/maik-emanoel.png",
+    comments: 10,
+    retweets: 1,
+    likes: 2023,
   },
   {
     userAvatar: "https://github.com/diego3g.png",
     userName: "Diego Fernandes",
-    userLogin: "diego_3g",
-    content: "Teste teste",
+    userLogin: "dieegosf",
+    content: "Testandooooooo",
     imageUrl: "https://source.unsplash.com/random",
+    comments: 15,
+    retweets: 4,
+    likes: 2023,
   },
   {
     userAvatar: "https://github.com/maykbrito.png",
     userName: "Mayk Brito",
     userLogin: "maykbrito",
-    content: "Falaaaaaa, dev!",
+    content: "Falaaaaaa, dev! Já participaram do #BoraCodar dessa semana?",
     imageUrl: undefined,
+    comments: 10,
+    retweets: 5,
+    likes: 2023,
+  },
+  {
+    userAvatar: "https://github.com/rocketseat.png",
+    userName: "Rocketseat",
+    userLogin: "rockeatseat",
+    content: `Estamos ao vivo! 👇 
+
+    https://youtube.com/live/1pFb0AOLp...
+    `,
+    imageUrl:"https://pbs.twimg.com/media/F2E7P52WMAAZMTN?format=jpg&name=small",
+    comments: 24,
+    retweets: 12,
+    likes: 1980,
+  },
+  {
+    userAvatar: "https://github.com/diego3g.png",
+    userName: "Diego Fernandes",
+    userLogin: "dieegosf",
+    content: "That's another test",
+    imageUrl: "https://source.unsplash.com/random",
+    comments: 6,
+    retweets: 9,
+    likes: 2005,
+  },
+  {
+    userAvatar: "https://github.com/rocketseat.png",
+    userName: "Rocketseat",
+    userLogin: "rockeatseat",
+    content: `Tem desafio novo do #boraCodar no ar! 
+
+    Vem criar um gerador de sugestão de filmes ⤵️
+   https://rseat.in/L2eq7wn8e
+   
+   #bolhadev
+    `,
+    imageUrl:"https://pbs.twimg.com/media/F1-iOr4XsAIwV2_?format=jpg&name=small",
+    comments: 56,
+    retweets: 20,
+    likes: 4374,
   },
 ];
