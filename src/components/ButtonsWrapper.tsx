80c31a058b70426f24e00b4770c32c045c1aa8b4
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-07 11:47:30 -0300

    feat: create function to handle increase likes

--- src/components/ButtonsWrapper.tsx
+++ src/components/ButtonsWrapper.tsx
@@ -5,6 +5,7 @@ import {
   ChartLine,
   Export,
 } from "@phosphor-icons/react";
+import { useState } from "react";
 
 interface ButtonsWrapperProps {
   comments: number | undefined;
@@ -16,17 +17,20 @@ interface ButtonProps {
   icon: React.ElementType;
   text: string | number;
   hoverColorClass: string;
+  handleIncreaseLike?: () => void
+  isLiked?: boolean
 }
 
-function Button({ icon: Icon, text, hoverColorClass }: ButtonProps) {
+function Button({ icon: Icon, text, hoverColorClass, handleIncreaseLike, isLiked }: ButtonProps) {
   return (
     <button
-      className={`flex items-center gap-2 text-sm text-[#89a2b8] hover:text-${hoverColorClass} group`}
+      className={`flex items-center gap-2 text-sm text-[#89a2b8] hover:text-${hoverColorClass} group ${isLiked ? 'text-likePink' : ''}`}
+      onClick={handleIncreaseLike}
     >
       <div
         className={`w-[34.75px] h-[34.75px] grid place-items-center rounded-full -m-2 transition-colors duration-200 group-hover:bg-${hoverColorClass}/10`}
       >
-        <Icon size={18.75} />
+        {!isLiked ? <Icon size={18.75} /> : <Icon size={18.75} weight="fill" />}
       </div>
       <span className="pl-1">{text}</span>
     </button>
@@ -38,6 +42,16 @@ export function ButtonsWrapper({
   retweets,
   likes,
 }: ButtonsWrapperProps) {
+    const [initialLikes, setInitialLikes] = useState(likes)
+    const [isLiked, setIsLiked] = useState(false)
+
+    function handleIncreaseLike() {
+        if(isLiked) return
+
+        setInitialLikes((prevState) => (prevState ?? 2004) + 1)
+        setIsLiked(true)
+    }
+
   return (
     <div
       className="flex items-center mt-3 justify-between h-5"
@@ -55,8 +69,10 @@ export function ButtonsWrapper({
       />
       <Button
         icon={Heart}
-        text={likes ? likes : 2004}
+        text={initialLikes ? initialLikes : 2004}
         hoverColorClass="likePink"
+        handleIncreaseLike={handleIncreaseLike}
+        isLiked={isLiked}
       />
       <Button 
         icon={ChartLine} 
