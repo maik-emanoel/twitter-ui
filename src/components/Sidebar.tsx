3c91bc6e31d85981d7116da9f2c6d65baff6193e
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-12 15:21:17 -0300

    add new content

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,6 +2,7 @@ import twitterLogo from "../assets/logo-twitter.svg";
 import {
   Bell,
   BookmarkSimple,
+  DotsThree,
   DotsThreeCircle,
   Envelope,
   FileText,
@@ -13,37 +14,68 @@ import {
 import { SidebarLink } from "./SidebarLink";
 
 export function Sidebar() {
+  const isMobile = window.matchMedia("(max-width: 768px)").matches;
+
   return (
-    <aside className="py-6 px-5 flex flex-col gap-8 md:items-center h-screen sticky top-0 sm:hidden">
-      <img src={twitterLogo} alt="Logo" className="w-8 h-8" />
+    <aside className="pt-6 px-5 flex flex-col justify-between md:items-center h-screen sticky top-0 md:px-0 sm:hidden">
+      <div className="flex flex-col gap-8 md:items-center">
+        <img src={twitterLogo} alt="Logo" className="w-8 h-8" />
 
-      <nav className="flex flex-col gap-2">
-        <SidebarLink
-          path="/"
-          icon={<House size={32} weight="fill" />}
-          text="Home"
-        />
-        <SidebarLink path="" icon={<MagnifyingGlass size={32} />} text="Explorer" />
-        <SidebarLink path="" icon={<Bell size={32} />} text="Notifications" />
-        <SidebarLink path="" icon={<Envelope size={32} />} text="Messages" />
-        <SidebarLink
-          path=""
-          icon={<BookmarkSimple size={32} />}
-          text="Bookmarks"
-        />
-        <SidebarLink path="" icon={<FileText size={32} />} text="Lists" />
-        <SidebarLink path="" icon={<User size={32} />} text="Profile" />
-        <SidebarLink
-          path=""
-          icon={<DotsThreeCircle size={32} />}
-          text="More"
-        />
-      </nav>
+        <nav className="flex flex-col gap-2">
+          <SidebarLink
+            path="/"
+            icon={<House size={32} weight="fill" />}
+            text="Home"
+          />
+          <SidebarLink
+            path=""
+            icon={<MagnifyingGlass size={32} />}
+            text="Explorer"
+          />
+          <SidebarLink path="" icon={<Bell size={32} />} text="Notifications" />
+          <SidebarLink path="" icon={<Envelope size={32} />} text="Messages" />
+          <SidebarLink
+            path=""
+            icon={<BookmarkSimple size={32} />}
+            text="Bookmarks"
+          />
+          <SidebarLink path="" icon={<FileText size={32} />} text="Lists" />
+          <SidebarLink path="" icon={<User size={32} />} text="Profile" />
+          <SidebarLink
+            path=""
+            icon={<DotsThreeCircle size={32} />}
+            text="More"
+          />
+        </nav>
 
-      <button className="bg-twitterBlue rounded-full p-4 flex justify-center w-full text-white text-xl font-black hover:brightness-90 md:p-2">
-        <Pencil className="w-6 h-6 hidden md:block" />
-        <span className="md:hidden">Tweet</span>
-      </button>
+        <button className="bg-twitterBlue rounded-full p-4 flex justify-center w-full text-white text-xl font-black hover:brightness-90 md:p-2 md:w-10 md:h-10">
+          <Pencil className="w-6 h-6 hidden md:block" />
+          <span className="md:hidden">Tweet</span>
+        </button>
+      </div>
+
+      <div
+        className={`p-4 flex items-center gap-3 my-3 rounded-full transition-colors duration-200 select-none cursor-pointer ${
+          !isMobile
+            ? "hover:bg-zinc-100 hover:dark:bg-zinc-800"
+            : ''
+        } md:w-16 md:h-16 md:p-3`}
+      >
+        <img
+          src="https://github.com/maik-emanoel.png"
+          alt="Foto de perfil do usuário"
+          className="w-10 h-10 rounded-full"
+        />
+        <div className="flex flex-1 justify-between items-center md:hidden">
+          <div className="flex flex-col flex-shrink-0 text-sm">
+            <span className="font-bold">Maik</span>
+            <span className="opacity-70 dark:opacity-50">@maik_emanoel</span>
+          </div>
+          <div>
+            <DotsThree weight="bold" size={18.75} />
+          </div>
+        </div>
+      </div>
     </aside>
   );
 }
