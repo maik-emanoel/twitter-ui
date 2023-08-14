08b084c3e35572b14b8bf34086ce2962efa1d658
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-14 11:22:06 -0300

    refactor

--- src/components/SidebarLink.tsx
+++ src/components/SidebarLink.tsx
@@ -14,9 +14,7 @@ export function SidebarLink({ path, icon: Icon, text }: SidebarLinkProps) {
   return (
     <NavLink
       to={path}
-      className={`flex items-center gap-5 w-fit text-xl font-bold rounded-full py-2 pl-2 pr-6 hover:bg-zinc-100 hover:dark:bg-zinc-800 md:p-2 ${
-        isActive ? "active" : ""
-      }`}
+      className="flex items-center gap-5 w-fit text-xl font-bold rounded-full py-2 pl-2 pr-6 hover:bg-zinc-100 hover:dark:bg-zinc-800 md:p-2"
     >
       <Icon size={32} weight={isActive ? "fill" : "regular"} />
       <span className="md:hidden">{text}</span>
