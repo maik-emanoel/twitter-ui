a1202c0fbcda4342201300e3418b2fda3d389351
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-14 11:19:37 -0300

    refactor & solve problem with active class

--- src/components/Bottombar.tsx
+++ src/components/Bottombar.tsx
@@ -1,23 +1,34 @@
-import { Bell, Envelope, House, MagnifyingGlass } from "@phosphor-icons/react";
-import { NavLink } from "react-router-dom";
+import { Bell, Envelope, House, IconProps, MagnifyingGlass } from "@phosphor-icons/react";
+import { NavLink, useLocation } from "react-router-dom";
+
+interface BottombarLinkProps {
+  to: string;
+  icon: React.ElementType<IconProps>;
+}
+
+export function BottombarLink({ to, icon: Icon }: BottombarLinkProps) {
+  const location = useLocation();
+  const isActive = location.pathname === to;
+
+  return (
+    <NavLink
+      to={to}
+      className="flex items-center h-full justify-center"
+    >
+      <Icon size={24} weight={isActive ? "fill" : "regular"} />
+    </NavLink>
+  );
+}
 
 export function Bottombar() {
-    return (
-        <nav className="hidden fixed bottom-0 w-full h-14 bg-white border-t-[1px] border-grayBorder dark:bg-bodyDark dark:border-grayBorderDark sm:block">
-            <div className="flex items-center h-full justify-around">
-                <NavLink to="/"> 
-                    <House size={24} weight="fill"/>
-                </NavLink>
-                <NavLink to="/"> 
-                    <MagnifyingGlass size={24} />
-                </NavLink>
-                <NavLink to="/"> 
-                    <Bell size={24} />
-                </NavLink>
-                <NavLink to="/"> 
-                    <Envelope size={24} />
-                </NavLink>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="hidden fixed bottom-0 w-full h-14 bg-white border-t-[1px] border-grayBorder dark:bg-bodyDark dark:border-grayBorderDark sm:block">
+      <div className="flex items-center h-full justify-around">
+        <BottombarLink to="/" icon={House} />
+        <BottombarLink to="/explorer" icon={MagnifyingGlass} />
+        <BottombarLink to="/notifications" icon={Bell} />
+        <BottombarLink to="/messages" icon={Envelope} />
+      </div>
+    </nav>
+  );
 }
