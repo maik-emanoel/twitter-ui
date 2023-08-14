659ab04546db86adc9000b0fbb18ab9fbc57e9ae
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-14 11:11:12 -0300

    solve problem with active class

--- src/components/SidebarLink.tsx
+++ src/components/SidebarLink.tsx
@@ -1,16 +1,24 @@
-import { ReactNode } from "react";
-import { NavLink } from "react-router-dom";
+import { IconProps } from "@phosphor-icons/react";
+import { NavLink, useLocation } from "react-router-dom";
 
 interface SidebarLinkProps {
-    path: string,
-    icon: ReactNode,
-    text: string
+  path: string;
+  icon: React.ElementType<IconProps>;
+  text: string;
 }
 
-export function SidebarLink( {path, icon, text} :SidebarLinkProps ) {
+export function SidebarLink({ path, icon: Icon, text }: SidebarLinkProps) {
+  const location = useLocation(); 
+  const isActive = location.pathname === path; 
+
   return (
-    <NavLink to={path} className="flex items-center gap-5 w-fit text-xl font-bold rounded-full py-2 pl-2 pr-6 hover:bg-zinc-100 hover:dark:bg-zinc-800 md:p-2">
-      {icon}
+    <NavLink
+      to={path}
+      className={`flex items-center gap-5 w-fit text-xl font-bold rounded-full py-2 pl-2 pr-6 hover:bg-zinc-100 hover:dark:bg-zinc-800 md:p-2 ${
+        isActive ? "active" : ""
+      }`}
+    >
+      <Icon size={32} weight={isActive ? "fill" : "regular"} />
       <span className="md:hidden">{text}</span>
     </NavLink>
   );
