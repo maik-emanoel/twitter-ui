22bdeff370ced236b2323d88727f912a75de733e
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-14 11:10:47 -0300

    add path to all links

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -19,33 +19,29 @@ export function Sidebar() {
   return (
     <aside className="pt-6 px-5 flex flex-col justify-between md:items-center h-screen sticky top-0 md:px-0 sm:hidden">
       <div className="flex flex-col gap-8 md:items-center">
-        <img src={twitterLogo} alt="Logo" className="w-8 h-8 ml-2 cursor-pointer" />
+        <img
+          src={twitterLogo}
+          alt="Logo"
+          className="w-8 h-8 ml-2 cursor-pointer"
+        />
 
         <nav className="flex flex-col gap-1">
+          <SidebarLink path="/" icon={House} text="Home" />
           <SidebarLink
-            path="/"
-            icon={<House size={32} weight="fill" />}
-            text="Home"
-          />
-          <SidebarLink
-            path=""
-            icon={<MagnifyingGlass size={32} />}
+            path="/explorer"
+            icon={MagnifyingGlass}
             text="Explorer"
           />
-          <SidebarLink path="" icon={<Bell size={32} />} text="Notifications" />
-          <SidebarLink path="" icon={<Envelope size={32} />} text="Messages" />
+          <SidebarLink path="/notifications" icon={Bell} text="Notifications" />
+          <SidebarLink path="/messages" icon={Envelope} text="Messages" />
           <SidebarLink
-            path=""
-            icon={<BookmarkSimple size={32} />}
+            path="/bookmarks"
+            icon={BookmarkSimple}
             text="Bookmarks"
           />
-          <SidebarLink path="" icon={<FileText size={32} />} text="Lists" />
-          <SidebarLink path="" icon={<User size={32} />} text="Profile" />
-          <SidebarLink
-            path=""
-            icon={<DotsThreeCircle size={32} />}
-            text="More"
-          />
+          <SidebarLink path="/lists" icon={FileText} text="Lists" />
+          <SidebarLink path="/profile" icon={User} text="Profile" />
+          <SidebarLink path="/more" icon={DotsThreeCircle} text="More" />
         </nav>
 
         <button className="bg-twitterBlue rounded-full flex justify-center items-center w-full h-14 text-white text-xl font-black hover:brightness-90 md:p-2 md:w-10 md:h-10">
