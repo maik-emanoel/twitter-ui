07d9697e3adffa1cb29538425af9ecd8e79e3895
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-15 10:51:01 -0300

    add children routes to notifications

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,27 +1,38 @@
-import { createBrowserRouter  } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { Timeline } from "./pages/Timeline";
 import { Status } from "./pages/Status";
 import { Default } from "./layouts/Default";
 import { ErrorPage } from "./pages/ErrorPage";
+import { Notifications } from "./pages/Notifications";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Default />,
+  {
+    path: "/",
+    element: <Default />,
+    children: [
+      {
+        path: "/",
+        element: <Timeline />,
+      },
+      {
+        path: "/status",
+        element: <Status />,
+      },
+      {
+        path: "/notifications",
+        element: <Notifications />,
         children: [
-            {
-                path: '/',
-                element: <Timeline />
-            },
-            {
-                path: '/status',
-                element: <Status />
-            },
-            {
-                path: '/notifications',
-                element: <h1>Notificações</h1>
-            }
+          {
+            path: "/notifications/verified",
+            element: <h1>Olá</h1>,
+          },
+          {
+            path: "/notifications/mentions",
+            element: <h1>Oi</h1>,
+          },
         ],
-        errorElement: <ErrorPage />
-    }
-])
+      },
+    ],
+    errorElement: <ErrorPage />,
+  },
+]);
