8f8c3a1c62019404ee386743f83eb851562b4695
Maik Emanoel <maikemanoel11@gmail.com>
2023-09-18 11:24:38 -0300

    wrap all application with TweetProvider

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -2,19 +2,22 @@ import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components/Sidebar";
 import { Bottombar } from "../components/Bottombar";
 import { SidebarProvider } from "../context/SidebarContext";
+import { TweetProvider } from "../context/TweetContext";
 
 export function Default() {
   return (
     <div className="mx-auto max-w-[1000px] grid grid-cols-[300px_1fr] md:grid-cols-[80px_1fr] min-h-screen sm:grid-cols-1">
-      <SidebarProvider>
-        <Sidebar />
+      <TweetProvider>
+        <SidebarProvider>
+          <Sidebar />
 
-        <div className="border-l-[1px] border-r-[1px] border-grayBorder dark:border-grayBorderDark sm:border-none">
-          <Outlet />
-        </div>
+          <div className="border-l-[1px] border-r-[1px] border-grayBorder dark:border-grayBorderDark sm:border-none">
+            <Outlet />
+          </div>
 
-        <Bottombar />
-      </SidebarProvider>
+          <Bottombar />
+        </SidebarProvider>
+      </TweetProvider>
     </div>
   );
 }
