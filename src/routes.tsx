b8577abef02607a5f13d5e0f5a7cd99f4a7f1ba3
Maik Emanoel <maikemanoel11@gmail.com>
2023-09-18 11:19:38 -0300

    add id parameter on status route

--- src/routes.tsx
+++ src/routes.tsx
@@ -22,7 +22,7 @@ export const router = createBrowserRouter([
         element: <Timeline />,
       },
       {
-        path: "/status",
+        path: "/status/:id",
         element: <Status />,
       },
       {
