fcae0897a9047f31879071d180cdf8ff8f1e6b83
Maik Emanoel <maikemanoel11@gmail.com>
2023-10-02 11:39:48 -0300

    add tw-merge & children

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,17 @@
 import { Moon, Sparkle, SunDim } from "@phosphor-icons/react";
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import twitterLogo from "../assets/logo-twitter.svg";
 import { useSidebarContext } from "../context/SidebarContext";
 import { loadDarkModeValue, saveDarkModeValue } from "../utils/darkModeUtils";
 import { isTouchSupported } from "../utils/touchUtils";
+import { twMerge } from "tailwind-merge";
 
-interface HeaderProps {
+interface HeaderProps extends React.HTMLProps<HTMLDivElement> {
   title: string;
+  children?: ReactNode;
 }
 
-export function Header({ title }: HeaderProps) {
+export function Header({ title, children, className }: HeaderProps) {
   const [isDark, setIsDark] = useState<boolean>(loadDarkModeValue());
   const { handleShowMobileSidebar } = useSidebarContext();
 
@@ -41,41 +43,52 @@ export function Header({ title }: HeaderProps) {
   }, [isDark]);
 
   return (
-    <div className="py-6 px-5 flex items-center justify-between text-xl font-bold border-b-[1px] border-b-grayBorder bg-white/75 sticky top-0 backdrop-blur-md z-10 dark:border-b-grayBorderDark dark:text-tweetColor dark:bg-bodyDark/60 sm:py-3">
-      <div className="hidden sm:block" onClick={handleShowSidebar}>
-        <img
-          src="https://github.com/maik-emanoel.png"
-          alt="Maik Emanoel"
-          className="w-8 h-8 rounded-full"
-        />
-      </div>
-      <span className="sm:hidden">{title}</span>
+    <div
+      className={twMerge(
+        "py-6 px-5 h-[76px] flex items-center justify-between text-xl font-bold border-b border-b-grayBorder bg-white/75 sticky top-0 backdrop-blur-md z-10 dark:border-b-grayBorderDark dark:text-tweetColor dark:bg-bodyDark/60 sm:py-3",
+        className
+      )}
+    >
+      {children ? (
+        children
+      ) : (
+        <>
+          <div className="hidden sm:block" onClick={handleShowSidebar}>
+            <img
+              src="https://github.com/maik-emanoel.png"
+              alt="Maik Emanoel"
+              className="w-8 h-8 rounded-full"
+            />
+          </div>
+          <span className="sm:hidden">{title}</span>
 
-      <img
-        src={twitterLogo}
-        alt="Logo do Twitter"
-        className="hidden w-6 h-6 sm:block"
-      />
-      <div className="flex items-center gap-3">
-        <button
-          data-istouchsupported={isTouchSupported}
-          onClick={handleToggleTheme}
-          className="outline outline-transparent outline-8 data-[istouchsupported=false]:hover:outline-twitterBlue/10 data-[istouchsupported=false]:hover:bg-twitterBlue/10 rounded-full"
-        >
-          {isDark ? (
-            <SunDim size={24} className="text-twitterBlue" />
-          ) : (
-            <Moon size={24} className="text-twitterBlue" />
-          )}
-        </button>
+          <img
+            src={twitterLogo}
+            alt="Logo do Twitter"
+            className="hidden w-6 h-6 sm:block"
+          />
+          <div className="flex items-center gap-3">
+            <button
+              data-istouchsupported={isTouchSupported}
+              onClick={handleToggleTheme}
+              className="outline outline-transparent outline-8 data-[istouchsupported=false]:hover:outline-twitterBlue/10 data-[istouchsupported=false]:hover:bg-twitterBlue/10 rounded-full"
+            >
+              {isDark ? (
+                <SunDim size={24} className="text-twitterBlue" />
+              ) : (
+                <Moon size={24} className="text-twitterBlue" />
+              )}
+            </button>
 
-        <button
-          data-istouchsupported={isTouchSupported}
-          className="outline outline-transparent outline-8 data-[istouchsupported=false]:hover:outline-twitterBlue/10 data-[istouchsupported=false]:hover:bg-twitterBlue/10 rounded-full sm:hidden"
-        >
-          <Sparkle size={24} className="text-twitterBlue" />
-        </button>
-      </div>
+            <button
+              data-istouchsupported={isTouchSupported}
+              className="outline outline-transparent outline-8 data-[istouchsupported=false]:hover:outline-twitterBlue/10 data-[istouchsupported=false]:hover:bg-twitterBlue/10 rounded-full sm:hidden"
+            >
+              <Sparkle size={24} className="text-twitterBlue" />
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
