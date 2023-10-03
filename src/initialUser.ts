7633a3e555bcc66349622bc88952211ca459c0e1
Maik Emanoel <maikemanoel11@gmail.com>
2023-10-03 19:14:08 -0300

    create initialUser

---
+++ src/initialUser.ts
@@ -0,0 +1,9 @@
+export const initialUser = {
+    name: 'Maik Emanoel',
+    login: 'maik_emanoel',
+    avatarURL: 'https://github.com/maik-emanoel.png',
+    bio: 'Loading...',
+    followers: 0,
+    following: 3,
+    created_at: 'July 2022'
+}
