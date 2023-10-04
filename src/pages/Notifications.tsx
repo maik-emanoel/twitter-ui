4fd52722eebfe8337fcf4033541994b9bf4d7107
Maik Emanoel <maikemanoel11@gmail.com>
2023-10-04 09:21:12 -0300

    turn NotificationsLink into Link and remove it

--- src/pages/Notifications.tsx
+++ src/pages/Notifications.tsx
@@ -1,45 +1,15 @@
-import { NavLink, Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
-import { isTouchSupported as touchIsSupported } from "../utils/touchUtils";
-
-interface NotificationsLinkProps {
-  path: string;
-  name: string;
-}
-
-function NotificationsLink({ path, name }: NotificationsLinkProps) {
-  const location = useLocation();
-  const isActive = location.pathname === path;
-
-  return (
-    <NavLink
-      to={path}
-      className={`w-full h-full grid place-items-center text-inherit relative ${
-        isActive ? "dark:text-white" : "text-zinc-400 dark:text-white/40"
-      } ${
-        touchIsSupported
-          ? ""
-          : "hover:bg-zinc-100 hover:dark:bg-zinc-800"
-      }`}
-    >
-      <span className={`text-sm ${isActive ? "font-bold" : "font-medium"}`}>
-        {name}
-      </span>
-      {isActive && (
-        <span className="absolute bottom-0 w-14 h-1 bg-twitterBlue rounded-full"></span>
-      )}
-    </NavLink>
-  );
-}
+import { Link } from "../components/Link";
 
 export function Notifications() {
   return (
     <>
       <Header title="Notifications" />
       <div className="flex h-[53px] items-center justify-around">
-        <NotificationsLink path="/notifications" name="All" />
-        <NotificationsLink path="/notifications/verified" name="Verified" />
-        <NotificationsLink path="/notifications/mentions" name="Mentions" />
+        <Link path="/notifications" name="All" />
+        <Link path="/notifications/verified" name="Verified" />
+        <Link path="/notifications/mentions" name="Mentions" />
       </div>
       <Outlet />
     </>
