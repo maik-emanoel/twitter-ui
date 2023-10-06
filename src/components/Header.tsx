3ecc3e08a59c9503e9c349d1ba3c1e4a53cf9985
Maik Emanoel <maikemanoel11@gmail.com>
2023-10-06 10:00:27 -0300

    decrease Header height on small screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -46,7 +46,7 @@ export function Header({ title, children, className }: HeaderProps) {
   return (
     <div
       className={twMerge(
-        "py-6 px-5 h-[76px] flex items-center justify-between text-xl font-bold border-b border-b-grayBorder bg-white/75 sticky top-0 backdrop-blur-md z-10 dark:border-b-grayBorderDark dark:text-tweetColor dark:bg-bodyDark/60 sm:py-3",
+        "py-6 px-5 h-[76px] flex items-center justify-between text-xl font-bold border-b border-b-grayBorder bg-white/75 sticky top-0 backdrop-blur-md z-10 dark:border-b-grayBorderDark dark:text-tweetColor dark:bg-bodyDark/60 sm:py-3 sm:h-16",
         className
       )}
     >
