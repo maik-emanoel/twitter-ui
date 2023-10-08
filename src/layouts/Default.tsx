e9fb7ba5d0f94dc4020406f83033bf7c90f68031
Maik Emanoel <maikemanoel11@gmail.com>
2023-10-08 12:38:48 -0300

    add ScrollDirectionProvider

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -3,19 +3,22 @@ import { Sidebar } from "../components/Sidebar";
 import { Bottombar } from "../components/Bottombar";
 import { SidebarProvider } from "../context/SidebarContext";
 import { TweetProvider } from "../context/TweetContext";
+import { ScrollDirectionProvider } from "../context/ScrollContext";
 
 export function Default() {
   return (
     <div className="mx-auto max-w-[1000px] grid grid-cols-[300px_1fr] md:grid-cols-[80px_1fr] min-h-screen sm:grid-cols-1">
       <TweetProvider>
         <SidebarProvider>
-          <Sidebar />
+          <ScrollDirectionProvider>
+            <Sidebar />
 
-          <div className="border-l border-r border-grayBorder dark:border-grayBorderDark sm:border-none">
-            <Outlet />
-          </div>
+            <div className="border-l border-r border-grayBorder dark:border-grayBorderDark sm:border-none">
+              <Outlet />
+            </div>
 
-          <Bottombar />
+            <Bottombar />
+          </ScrollDirectionProvider>
         </SidebarProvider>
       </TweetProvider>
     </div>
