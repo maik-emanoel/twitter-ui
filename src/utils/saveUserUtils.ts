a7457edf98222c65fbff49540b0ec1d90015f017
Maik Emanoel <maikemanoel11@gmail.com>
2023-10-19 09:22:32 -0300

    handle user info on local storage

---
+++ src/utils/saveUserUtils.ts
@@ -0,0 +1,22 @@
+import { UserInfo } from "../context/UserContext";
+
+export function saveUser(userInfo: UserInfo) {
+  localStorage.setItem("userInfo", JSON.stringify(userInfo));
+}
+
+export function loadUser() {
+  const storedUser = localStorage.getItem("userInfo");
+  return storedUser
+    ? (JSON.parse(storedUser) as UserInfo)
+    : {
+        userName: "",
+        userLogin: "",
+        avatar: "",
+        birthdayDate: {
+          month: null,
+          day: null,
+          year: null,
+        },
+        created_at: ''
+      };
+}
