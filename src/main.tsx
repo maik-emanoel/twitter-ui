832e225923f84a068ff37178f103e9e2abff1b59
Maik Emanoel <maikemanoel11@gmail.com>
2023-10-20 11:33:06 -0300

    import AppRoutes

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
 import { UserProvider } from "./context/UserContext";
+import { AppRoutes } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <UserProvider>
-      <RouterProvider router={router} />
+      <AppRoutes />
     </UserProvider>
   </React.StrictMode>
 );
